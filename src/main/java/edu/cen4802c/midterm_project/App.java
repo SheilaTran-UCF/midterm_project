d7f11fb3825c50ae90a244abc02882d040a1e5c5
SheilaTran-UCF <minhngocny@hotmail.com>
2026-02-19 02:23:40 +0700

    Initial midterm pipeline project

---
+++ src/main/java/edu/cen4802c/midterm_project/App.java
@@ -0,0 +1,11 @@
+package edu.cen4802c.midterm_project;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Midterm Pipeline Demo Running Successfully ✅");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
